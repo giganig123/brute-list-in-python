6174c2c685e6334aa030ed8745891f5a2e19abec
giganig123 <gag430597@gmail.com>
2025-05-04 12:11:27 +0530

    bruteforcer.py
    still under development

---
+++ bruteforcer list.py
@@ -0,0 +1,9 @@
+# a python program which password list for brute forcing and leaked passwords
+lst1=("123456,12345678,1234,qwerty,12345,dragon,pussy,baseball,football,letmein,monkey,696969,abc123,password,123456,12345678,1234,qwerty,12345,dragon,pussy,baseball,football")
+lst2=("122,344,5,6,6,6,ceee12,fucker,gay,lordson,lightspeed,imgay,fuckme,gaylordfocker,wifi,sexy,slimshady,weff,qv,efv,ghrughru,fygeiufef,f68w7,6d367,rg4r5yh,jkyjr,")
+input("type an option lst1,lst2:")
+if input==lst1:
+    print(lst1)
+else:
+    print(lst2)
+print("copy and paste the list in a notepad and save as .txt to work")
